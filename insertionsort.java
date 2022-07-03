3df7c883d417d787f64ec811cb3943981e4f34b9
Andrew Lee <andrewlee@dev.myfiosgateway.com>
2022-07-03 12:02:25 -0400

    insertion sort progress

---
+++ insertionsort.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+
+public class insertionsort {
+
+    public static void main(String args[]) {
+        int[] arr = {10, 20, 30, 25, 40};
+
+        System.out.println(Arrays.toString(arr));
+
+        for (int i = 0; i < arr.length; i++) {
+
+        }
+        System.out.println(Arrays.toString(arr));
+    }
+}

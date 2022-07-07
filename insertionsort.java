2f2dc2e66652ff1c2ef531a142bba7501626ccf8
Andrew Lee <dev@Andrews-MBP.ssid2>
2022-07-07 14:51:37 -0400

    finished insertion sort

--- insertionsort.java
+++ insertionsort.java
@@ -7,7 +7,17 @@ public class insertionsort {
 
         System.out.println(Arrays.toString(arr));
 
-        for (int i = 0; i < arr.length; i++) {
+        int len = arr.length;
+        for (int i = 1; i < len; i++) {
+            int key = arr[i];
+            int j = i - 1;
+
+            while (j >= 0 && arr[i] < arr[j]) {
+                arr[j+1] = arr[j];
+                j = j-1;
+
+                arr[j+1] = key;
+            }
 
         }
         System.out.println(Arrays.toString(arr));

7b5ddf27e7b5ed9adcf8173dc972b13e1d73b09a
Andrew Lee <dev@Andrews-MBP.ssid2>
2022-07-07 15:00:12 -0400

    created and started linked list

---
+++ linkedlist.java
@@ -0,0 +1,16 @@
+import java.util.*; 
+
+public class linkedlist {
+
+    public static void main(String args[]) {
+        LinkedList<String> ll = new LinkedList<>();  
+
+        ll.add("Jim");
+        ll.add("Drew");
+        ll.add("Sue");
+        
+        System.out.println("Linked List: " + ll);
+
+
+    }
+}
